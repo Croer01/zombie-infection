770a1107934fe3a94462aa7c76fa049c3b7d252a
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-11-03 20:15:55 +0100

    improve enemies. Can move left or right and die

--- Enemy.h
+++ Enemy.h
@@ -4,38 +4,75 @@
 #include "globals.h"
 #include "Sprites.h"
 #include "Player.h"
+#include "SlimLib.h"
 
 class Enemy {
   private:
     static const int ENEMY_SPEED = 1;
 
     char *enemyImage;
-    byte x;
-    byte y;
+    boolean active;
+    Rect bounds;
 
   public:
     Enemy() {
       this->enemyImage = enenmyWalkRight;
-      x = 0;
-      y = 0;
+      bounds.setX(0);
+      bounds.setY(0);
+      bounds.setW(8);
+      bounds.setH(12);
     };
 
     void update(Player *player) {
-      if (x < player->getX())
-        x++;
-      else if (x > player->getX())
-        x--;
+      if (active) {
+        //move to player
+        if (bounds.getX() < player->getX()) {
+          bounds.setX(bounds.getX() + 1);
+          this->enemyImage = enenmyWalkRight;
+        } else if (bounds.getX() > player->getX()) {
+          bounds.setX(bounds.getX() - 1);
+          this->enemyImage = enenmyWalkLeft;
+        }
 
-      if (y < player->getY())
-        y++;
-      else if (y > player->getY())
-        y--;
+        if (bounds.getY() < player->getY())
+          bounds.setY(bounds.getY() + 1);
+        else if (bounds.getY() > player->getY())
+          bounds.setY(bounds.getY() - 1);
+
+        //check if hurt by bullet
+
+        size_t bulletsLength = 0;
+        Bullet *playerBullets = player->getBullets(bulletsLength);
+
+        for (int i = 0; i < bulletsLength; i++) {
+          if (playerBullets[i].isActive() && this->getBounds().pointIsInside(playerBullets[i].getPosition())) {
+            playerBullets[i].disable();
+            this->hurt();
+          }
+        }
+      }
     };
 
     void render() {
-      arduboy.fillRect(Camera::getInstance()->getX() + x - 1, Camera::getInstance()->getY() + y - 1, 10, 14, BLACK);
-      arduboy.drawBitmap(Camera::getInstance()->getX() + x, Camera::getInstance()->getY() +  y, enemyImage, 8, 12, WHITE);
+      if (active) {
+        arduboy.fillRect(Camera::getInstance()->getX() + bounds.getX() - 1, Camera::getInstance()->getY() + bounds.getY() - 1, bounds.getW() + 2, bounds.getH() + 2, BLACK);
+        arduboy.drawBitmap(Camera::getInstance()->getX() + bounds.getX(), Camera::getInstance()->getY() +  bounds.getY(), enemyImage, bounds.getW(), bounds.getH(), WHITE);
+      }
     };
+
+    void hurt() {
+      active = false;
+    }
+
+    Rect getBounds() {
+      return bounds;
+    };
+
+    void respawn(byte x, byte y) {
+      bounds.setX(x);
+      bounds.setY(y);
+      active = true;
+    }
 };
 
 #endif
